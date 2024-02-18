09c6e67fc07cb57a1eea72a0b24c6696406d9e3a
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-18 21:21:42 +0600

    facing some problem

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,40 @@
 const selectedSeat = document.getElementById("selected-seat");
+const seatLeft = document.getElementById("seats-left");
+const selectedSeatinfo = document.getElementById("selected-seat-info");
+const tableRow = document.createElement("tr");
+const tableData = document.createElement("td");
+const tableType = document.createElement("td");
+const tablePrice = document.createElement("td");
+const seatName = document.getElementById("seatName");
+const seatType = document.getElementById("seatType");
+const seatPrice = document.getElementById("seatPrice");
+let tableListCount = 0;
+// console.log(seatName, seatType, seatPrice);
+let seatLeftCount = 40;
 let selectedSeatCount = 0;
 
 const keyboard = document.querySelectorAll("kbd");
 for (const key of keyboard) {
   key.addEventListener("click", function () {
+    // MAKING BACKGROUND GREEN
     const keyStyle = key.style;
     keyStyle.backgroundColor = "#1DD100";
     keyStyle.color = "white";
     keyStyle.border = "none";
+    // INCREASING AND DECREASIGN SEAT COUNT
     selectedSeatCount += 1;
+    seatLeftCount = seatLeftCount - 1;
     selectedSeat.innerText = selectedSeatCount;
-    console.log(selectedSeatCount);
+    seatLeft.innerText = seatLeftCount;
+    // WORKING WITH SEAT DETAIL
+    tableData.innerText = key.innerText;
+    tableType.innerText = seatType.innerText;
+    tablePrice.innerText = seatPrice.innerText;
+    tableRow.appendChild(tableData);
+    tableRow.appendChild(tableType);
+    tableRow.appendChild(tablePrice);
+    selectedSeatinfo.appendChild(tableRow);
+    tableListCount++;
   });
 }
 // console.log(kbd[0].innerText);
